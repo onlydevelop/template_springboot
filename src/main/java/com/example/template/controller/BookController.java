1f85bf61195c315e3d6ca41a0cf39625f2f141a7
Dipanjan Bhowmik <onlydevelop@gmail.com>
2022-01-31 12:52:45 +0530

    Exposed custom timed matrix in prometheus

--- src/main/java/com/example/template/controller/BookController.java
+++ src/main/java/com/example/template/controller/BookController.java
@@ -2,6 +2,7 @@ package com.example.template.controller;
 
 import com.example.template.model.Book;
 import com.example.template.service.BookService;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ public class BookController {
     BookService bookService;
 
     @GetMapping(path = "/books")
+    @Timed(value = "books.time.get", description = "Time taken to return GET books request")
     public List<Book> getBooks() {
         return bookService.get();
     }
